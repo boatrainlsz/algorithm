82d4e6ee9283f6a9e20c6e84d3c642a6e4f115df
boatrain <yinger521999@gmail.com>
2020-12-27 23:14:35 +0800

    三路快排的代码

--- src/com/lsz/util/SortHelper.java
+++ src/com/lsz/util/SortHelper.java
@@ -1,11 +1,12 @@
 package com.lsz.util;
 
 import com.lsz.quicksort.QuickSort;
+import com.lsz.quicksort.QuickSort3Way;
 
 public class SortHelper {
     public static void main(String[] args) throws Exception {
-        int[] arr = ArrayUtil.generateDescendArray(1, 50000000);
-//        int[] arr = ArrayUtil.generateRandomArray(0, 0, 50000000);
+//        int[] arr = ArrayUtil.generateDescendArray(1, 50000000);
+        int[] arr = ArrayUtil.generateRandomArray(0, 0, 50000000);
         sort(arr, "QuickSort");
         sort(arr, "QuickSort3Way");
     }
@@ -14,6 +15,8 @@ public class SortHelper {
         long start = System.nanoTime();
         if ("QuickSort".equals(method)) {
             QuickSort.sort(arr);
+        } else if ("QuickSort3Way".equals(method)) {
+            QuickSort3Way.sort(arr);
         }
         if (!ArrayUtil.isSorted(arr)) {
             throw new Exception(method + " failed, " + "size=" + arr.length);
