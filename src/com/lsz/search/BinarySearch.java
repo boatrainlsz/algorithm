968e743cb7f71250433f4c902d658859573d20c1
boatrain <sizhi.luo@bangdao-tech.com>
2021-01-01 21:24:02 +0800

    二分查找的非递归实现

--- src/com/lsz/search/BinarySearch.java
+++ src/com/lsz/search/BinarySearch.java
@@ -4,19 +4,52 @@ public class BinarySearch {
     private BinarySearch() {
     }
 
-    public static <E extends Comparable<E>> int search(E[] data, E target) {
-        return search(data, 0, data.length - 1, target);
+    /**
+     * 递归
+     *
+     * @param data
+     * @param target
+     * @param <E>
+     * @return
+     */
+    public static <E extends Comparable<E>> int searchR(E[] data, E target) {
+        return searchR(data, 0, data.length - 1, target);
     }
 
-    private static <E extends Comparable<E>> int search(E[] data, int l, int r, E target) {
+    /**
+     * 非递归
+     *
+     * @param data
+     * @param target
+     * @param <E>
+     * @return
+     */
+    public static <E extends Comparable<E>> int search(E[] data, E target) {
+        int l = 0, r = data.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (data[mid].compareTo(target) == 0) {
+                return mid;
+            } else if (data[mid].compareTo(target) < 0) {
+                l = mid + 1;
+            } else if (data[mid].compareTo(target) > 0) {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private static <E extends Comparable<E>> int searchR(E[] data, int l, int r, E target) {
         if (l > r) return -1;
         int mid = l + (r - l) / 2;
         if (data[mid].compareTo(target) == 0) {
             return mid;
         }
         if (data[mid].compareTo(target) < 0) {
-            return search(data, mid + 1, r, target);
+            return searchR(data, mid + 1, r, target);
         }
-        return search(data, l, mid - 1, target);
+        return searchR(data, l, mid - 1, target);
     }
+
+
 }
