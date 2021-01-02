334621662b101efa4d3dee0e4f2bdceb4807a044
boatrain <yinger521999@gmail.com>
2021-01-02 23:05:04 +0800

    最小上界搜索

---
+++ src/com/lsz/search/UpperSearch.java
@@ -0,0 +1,60 @@
+package com.lsz.search;
+
+/**
+ * 二分搜索的变种：最小上界搜索，找大于target的最小值
+ * todo
+ */
+public class UpperSearch {
+    private UpperSearch() {
+    }
+
+    /**
+     * 递归
+     *
+     * @param data
+     * @param target
+     * @param <E>
+     * @return
+     */
+    public static <E extends Comparable<E>> int searchR(E[] data, E target) {
+        return searchR(data, 0, data.length - 1, target);
+    }
+
+    /**
+     * 非递归
+     *
+     * @param data
+     * @param target
+     * @param <E>
+     * @return
+     */
+    public static <E extends Comparable<E>> int search(E[] data, E target) {
+        int l = 0, r = data.length - 1;
+        //在data[l,r]中查找
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (data[mid].compareTo(target) == 0) {
+                return mid;
+            } else if (data[mid].compareTo(target) < 0) {
+                l = mid + 1;
+            } else if (data[mid].compareTo(target) > 0) {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private static <E extends Comparable<E>> int searchR(E[] data, int l, int r, E target) {
+        if (l > r) return -1;
+        int mid = l + (r - l) / 2;
+        if (data[mid].compareTo(target) == 0) {
+            return mid;
+        }
+        if (data[mid].compareTo(target) < 0) {
+            return searchR(data, mid + 1, r, target);
+        }
+        return searchR(data, l, mid - 1, target);
+    }
+
+
+}
