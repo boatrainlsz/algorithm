09b2908c52645e5c333399e4669b217a56d2bde9
boatrain <yinger521999@gmail.com>
2021-01-22 21:14:45 +0800

    邻接矩阵表示

--- src/com/lsz/graph/AdjMatrix.java
+++ src/com/lsz/graph/AdjMatrix.java
@@ -1,6 +1,7 @@
 package com.lsz.graph;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -16,8 +17,10 @@ public class AdjMatrix {
         File file = new File(filename);
         try (Scanner scanner = new Scanner(file)) {
             V = scanner.nextInt();
+            if (V < 0) throw new Exception("wrong V");
             adj = new int[V][V];
             E = scanner.nextInt();
+            if (E < 0) throw new Exception("wrong E");
             for (int i = 0; i < V; i++) {
                 int a = scanner.nextInt();
                 int b = scanner.nextInt();
@@ -33,6 +36,42 @@ public class AdjMatrix {
         AdjMatrix adjMatrix = new AdjMatrix("C:\\Users\\Amy\\Desktop\\lsz\\algorithm\\src\\com\\lsz\\graph\\a.txt");
         System.out.println(adjMatrix);
     }
+
+    public int getV() {
+        return V;
+    }
+
+    public int getE() {
+        return E;
+    }
+
+    public boolean hasEdge(int p, int q) {
+        validateVertex(p);
+        validateVertex(q);
+        return adj[p][q] == 1;
+    }
+
+    public ArrayList<Integer> adj(int p) {
+        validateVertex(p);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (adj[p][i] == 1) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public int degree(int p) {
+        return adj(p).size();
+    }
+
+    private void validateVertex(int p) {
+        if (p < 0 || p > V) {
+            throw new RuntimeException("wrong vertex:" + p);
+        }
+    }
+
 
     @Override
     public String toString() {
